f65dca1db8e4631c877fdf8a2563711a0bddb673
Dimasskuy <freefiremax12d@gmail.com>
2024-01-06 22:23:16 +0700

    Update owner-getsesi.js

--- plugins/owner-getsesi.js
+++ plugins/owner-getsesi.js
@@ -1,11 +1,11 @@
 let fs = require('fs')
 let handler = async (m, { conn, text }) => {
     m.reply('Tunggu Sebentar, Proses Getting File creds.json')
-    let sesi = await fs.readFileSync('./sessions/creds.json')
+    let sesi = await fs.readFileSync('./sesibot/creds.json')
     return await conn.sendMessage(m.chat, { document: sesi, mimetype: 'application/json', fileName: 'creds.json' }, { quoted: m })
 }
 handler.help = ['getsesi']
-handler.tags = ['internet']
+handler.tags = ['owner']
 handler.command = /^(getsesi)$/i
 
 handler.rowner = true
